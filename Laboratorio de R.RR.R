e4b4c90fbdac72c3d8fc8e249bdbbecefe2fb1a1
Dharius Cabrera <dharius_antonioo@outlook.com>
2026-02-24 10:10:15 -0600

    commit

---
+++ Laboratorio de R.RR.R
@@ -0,0 +1,20 @@
+#gastos totales 
+300+240+1527+400+1500+1833 
+celular<- 300
+celular 
+celular<- 300
+transporte<- 240
+comestibles<-1527
+gimnasio <-400
+alquiler <-1500
+otros <-1833 
+
+total <- celular + transporte +comestibles + gimnasio + alquiler + otros
+total * 5
+
+gastos <- c(celular, transporte, comestibles, gimnasio, alquiler, otros)
+
+barplot(gastos)
+
+sort(gastos)
+
